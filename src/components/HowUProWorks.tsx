dc7c5d81f48230ca810bc64d9d14b0bdf93d7e1f
Allendale Nato <anato2@academic.rrc.ca>
2025-07-27 07:29:27 -0500

    style: add grid responsiveness

--- src/components/HowUProWorks.tsx
+++ src/components/HowUProWorks.tsx
@@ -20,7 +20,7 @@ export default function HowUProWorks(): ReactElement {
       </div>
 
       {/* Cards */}
-      <div className="grid grid-cols-6 gap-y-16 gap-x-10 px-6 py-20 container mx-auto">
+      <div className="grid grid-col-1 md:grid-cols-6 gap-y-16 gap-x-10 px-6 py-20 container mx-auto">
         {/* Capture Movement */}
         <div className={`col-span-3  ${cardClass}`}>
           <CardDetails
