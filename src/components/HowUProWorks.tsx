8243e930500f1c08871e05fc3858137a3fd9dd8d
alcoranpaul <67833935+alcoranpaul@users.noreply.github.com>
2025-07-26 23:18:29 -0500

    Refactored the cards to use Card Details and added some optional styling via tailwind or style
    Closes #12
    Closes #11

--- src/components/HowUProWorks.tsx
+++ src/components/HowUProWorks.tsx
@@ -17,6 +17,7 @@ export default function HowUProWorks(): ReactElement {
         </p>
       </div>
 
+      {/* Cards */}
       <div className="grid grid-cols-6 gap-y-16 gap-x-10 w-4/5 mx-auto">
         {/* Capture Movement */}
         <div className={`col-span-3  ${cardClass}`}>
@@ -26,59 +27,48 @@ export default function HowUProWorks(): ReactElement {
             to track real-time biomechanics. No wearables. No setup. Just
             hit record."
             imgSrc="tracking.png"
-            imgClassName="rounded-lg w-7/12 mx-auto"
+            imgClassName="rounded-lg w-[50%] mx-auto"
+            contentClassName=" flex flex-col h-full gap-2"
           />
         </div>
 
         {/* Level Up Skills */}
         <div className={`col-span-3 h-64 overflow-visible ${cardClass} `}>
-          <div className="relative flex flex-row  h-full ">
-            <div className="flex flex-col gap-2 px-6 py-4 text-white w-2/3 justify-start items-start">
-              <h1 className="font-bold text-2xl mb-2">Level Up Skills</h1>
-              <p className="">
-                Each session adapts to your player’s age and level. Earn XP by
-                completing challenges, improving form, and unlocking new drills.
-              </p>
-            </div>
-
-            <img
-              src="level_up_skills.svg"
-              alt="level_up_skills image"
-              className="shadow-lg shadow-green-800 p-4"
-            />
-          </div>
+          <CardDetails
+            title="Level Up Skills"
+            description="Each session adapts to your player’s age and level. Earn XP by
+                completing challenges, improving form, and unlocking new drills."
+            imgSrc="level_up_skills.svg"
+            imgClassName="shadow-lg shadow-green-800 p-4"
+            headerClassName="flex flex-col gap-2 px-6 py-4 text-white w-2/3 justify-start items-start"
+            contentClassName=" flex flex-row  h-full"
+          />
         </div>
 
         {/* Get Rewarded */}
         <div className={`col-span-4 ${cardClass}`}>
-          <div className=" flex flex-row-reverse  h-full justify-start items-start gap-5">
-            <div className="flex flex-col gap-2 px-6 py-4 text-white  ">
-              <h1 className="font-bold text-2xl mb-2 ">Get Rewarded</h1>
-              <p className="">
-                Earn badges, unlock content, and show off your skills.
-              </p>
-            </div>
-
-            <img
-              src="badges.svg"
-              alt=""
-              className="w-4/9"
-              style={{
-                WebkitMaskImage: `
+          <CardDetails
+            title="Get Rewarded"
+            description="Earn badges, unlock exclusive content, and show off your skills. Share your progress, connect with a community, and take your understanding of the game to new heights. Discover new insights, learn from experts, and get recognized for your knowledge of soccer machine learning applications."
+            imgSrc="badges.svg"
+            imgClassName="w-[45%]"
+            imgStyle={{
+              WebkitMaskImage: `
               radial-gradient(circle at bottom right, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0) 100%),
               radial-gradient(circle at top right, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 100%),
               linear-gradient(to left, rgba(0,0,0,0.8) 10%, rgba(0,0,0,0) 100%)
             `,
-                maskImage: `
+              maskImage: `
               radial-gradient(circle at bottom right, rgba(0,0,0,0.1) 90%, rgba(0,0,0,0) 100%),
               radial-gradient(circle at top right, rgba(0,0,0,0.8) 30%, rgba(0,0,0,0) 100%),
               linear-gradient(to left, rgba(0,0,0,0.1) 10%, rgba(0,0,0,0) 100%)
             `,
-                WebkitMaskRepeat: "no-repeat",
-                maskRepeat: "no-repeat",
-              }}
-            />
-          </div>
+              WebkitMaskRepeat: "no-repeat",
+              maskRepeat: "no-repeat",
+            }}
+            headerClassName="flex flex-col gap-2 px-6 py-4 text-white w-[55%]"
+            contentClassName="flex flex-row-reverse h-full justify-start items-start gap-5"
+          />
         </div>
 
         {/* Train Together */}
@@ -95,24 +85,48 @@ export default function HowUProWorks(): ReactElement {
   );
 }
 
+/**
+ * Properties for the CardDetails component
+ * @typedef {Object} CardDetailsProps
+ * @property {string} title - The title of the card
+ * @property {string} description - The description of the card
+ * @property {string} imgSrc - The source of the image
+ * @property {string} imgClassName - The class name of the image
+ * @property {React.CSSProperties} imgStyle - The style of the image
+ */
 type CardDetailsProps = {
   title: string;
   description: string;
   imgSrc?: string;
   imgClassName?: string;
-  style?: CSSProperties; // style is an object here
+  imgStyle?: CSSProperties;
+  headerClassName?: string;
+  contentClassName?: string;
 };
 
+/**
+ * A component to display a card with a title, description, and an optional image.
+ * @param {string} title - The title of the card
+ * @param {string} description - The description of the card
+ * @param {string} [imgSrc] - The source of the image
+ * @param {string} [imgClassName=""] - The class name of the image
+ * @param {React.CSSProperties} [imgStyle={}] - The style of the image
+ * @param {string} [headerClassName=""] - The class name of the header
+ * @param {string} [contentClassName=""] - The class name of the content
+ * @returns {ReactElement} - A React element representing the component
+ */
 function CardDetails({
   title,
   description,
   imgSrc,
   imgClassName = "",
-  style = {},
+  imgStyle = {},
+  headerClassName = "",
+  contentClassName = "",
 }: CardDetailsProps): ReactElement {
   return (
-    <div className="flex flex-col h-full gap-5">
-      <div className="px-6 py-4 flex flex-col gap-2">
+    <div className={`${contentClassName || "flex flex-col h-full gap-5"}`}>
+      <div className={`${headerClassName || "px-6 py-4 flex flex-col gap-2"}`}>
         <h1 className="font-bold text-white text-2xl mb-2">{title}</h1>
         <p className="text-white">{description}</p>
       </div>
@@ -121,7 +135,7 @@ function CardDetails({
           src={imgSrc}
           alt={`${imgSrc}_image`}
           className={`shadow-2xl shadow-green-400 ${imgClassName}`}
-          style={style} // directly pass object here
+          style={imgStyle} // directly pass object here
         />
       ) : (
         <div className="bg-white grow"></div>
